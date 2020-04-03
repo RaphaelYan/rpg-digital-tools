292ddd85107cafa68d8e219944552f2904ab839b
Raphael Yan <raphael@donkeycode.com>
2020-04-03 12:30:56 +0200

    add rule

--- src/app/dd-rules/dd-rules.component.ts
+++ src/app/dd-rules/dd-rules.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dd-rules',
   templateUrl: './dd-rules.component.html',
   styleUrls: ['./dd-rules.component.scss']
 })
-export class DdRulesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class DdRulesComponent {
+  public rules = [
+    {
+      title: 'Lanceur de sort versatil',
+      body: `Le personnage peut lancer 1 sort en dépensant 2 emplacements de niveau inférieur
+<br><br>
+Exemple : Lancer un sort de niveau 2 en dépensant 2 emplacement de sort niveau 1`
+    }, {
+      title: 'Attaquer sur la défensive',
+      body: `Permet de faire l'action "Esquiver", mais en pouvant attaquer avec un désavantage`
+    }, {
+      title: 'Défense totale',
+      body: `En prenant l'action "Esquiver", ajoute +2 à la classe d'armure`
+    }, {
+      title: 'Guérison naturelle',
+      body: `Les joueurs ne regagnent pas TOUS leurs pv lors d'un repos long.
+<br><br>
+Ils doivent dépenser leurs dés de vies comme lors d'un repos court pour se soigner naturellement.`
+    }
+  ];
 }
