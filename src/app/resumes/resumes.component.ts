2ed58d6bf78e8004442223e8d081e32d9aadc27b
Raphael Yan <raphael@donkeycode.com>
2020-04-07 16:50:41 +0200

    last agme

--- src/app/resumes/resumes.component.ts
+++ src/app/resumes/resumes.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
 export class ResumesComponent {
   public resumes = [
     {
+      date: '4 Avril 2020',
+      title: ' Les Foudres du Roi-Tonnerre - partie 6'
+    }, {
       date: '1 Avril 2020',
       title: ' Les Foudres du Roi-Tonnerre - partie 5'
     }, {
