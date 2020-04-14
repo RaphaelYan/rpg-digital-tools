a52fd949356073e5729c8726090300cd63ec651f
Raphael Yan <raphael@donkeycode.com>
2020-04-14 19:08:58 +0200

    add save to escapes

--- src/app/escape/escape.component.ts
+++ src/app/escape/escape.component.ts
@@ -7,36 +7,51 @@ import { Component } from '@angular/core';
 })
 export class EscapeComponent {
   public cardsClicked = {};
-  public escapes = [
+  public selectedGame = null;
+  public games = [
     {
-      title: 'Unlock',
-      slug: 'unlock',
-      rules: 'https://print-and-play.asmodee.fun/files/unlock/unlock_rules_fr.pdf',
-      games: [
-        {
-          title: 'Le temple de RA',
-          slug: 'temple-ra',
-          cards: ['init', '55', '24', 'A', 'R', '88', '20', '30', '6', '37', '8', '15', '66', '22', '91', '60', '9', '23', '28', '85', 'B', 'H', '39', '42']
-        }, {
-          title: 'A la poursuite de cabrakan',
-          slug: 'cabrakan',
-          cards: ['tuto', 'title', 'init', '6', '10', '12', '15', '18', '20', '24', '26', '30', '35', '38', '43', '46', '54', '59', '60', '75', '85', '95']
-        }
-      ]
+      title: 'Le temple de RA',
+      slug: 'temple-ra',
+      difficulty: 3,
+      cards: ['init', '55', '24', 'A', 'R', '88', '20', '30', '6', '37', '8', '15', '66', '22', '91', '60', '9', '23', '28', '85', 'B', 'H', '39', '42']
+    }, {
+      title: 'A la poursuite de cabrakan',
+      slug: 'cabrakan',
+      difficulty: 2,
+      cards: ['tuto', 'title', 'init', '6', '10', '12', '15', '18', '20', '24', '26', '30', '35', '38', '43', '46', '54', '59', '60', '75', '85', '95']
     }
   ];
-  public selectedEscape;
-  public selectedGame;
 
-  public selectEscape(escape): void {
-    this.selectedEscape = escape;
+  constructor() {
+    try {
+      const selectedGameSlug = localStorage.getItem('escape.selectedGame');
+      if (selectedGameSlug) {
+        const find = this.games.find((game) => game.slug === selectedGameSlug);
+        if (find) {
+          this.selectedGame = find;
+          const cardsClicked = localStorage.getItem('escape.cardsClicked.' + this.selectedGame.slug);
+          if (cardsClicked) {
+            this.cardsClicked = JSON.parse(cardsClicked);
+          }
+        }
+      }
+    } catch {}
   }
 
-  public selectGame(game): void {
+  public selectGame(game: any): void {
     this.selectedGame = game;
+    localStorage.setItem('escape.selectedGame', game.slug);
+
+    const cardsClicked = localStorage.getItem('escape.cardsClicked.' + this.selectedGame.slug);
+    if (cardsClicked) {
+      this.cardsClicked = JSON.parse(cardsClicked);
+    } else {
+      this.cardsClicked = {};
+    }
   }
 
-  public cardClicked(card) {
+  public cardClicked(card: any) {
     this.cardsClicked[card] = !this.cardsClicked[card];
+    localStorage.setItem('escape.cardsClicked.' + this.selectedGame.slug, JSON.stringify(this.cardsClicked));
   }
 }
