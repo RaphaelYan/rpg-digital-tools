317e74d3a479fb27a152e4ff265449aeb477059e
Raphael Yan <raphael@donkeycode.com>
2020-04-14 15:38:11 +0200

    escape 2

--- src/app/escape/escape.component.ts
+++ src/app/escape/escape.component.ts
@@ -16,6 +16,10 @@ export class EscapeComponent {
           title: 'Le temple de RA',
           slug: 'temple-ra',
           cards: ['init', '55', '24', 'A', 'R', '88', '20', '30', '6', '37', '8', '15', '66', '22', '91', '60', '9', '23', '28', '85', 'B', 'H', '39', '42']
+        }, {
+          title: 'A la poursuite de cabrakan',
+          slug: 'cabrakan',
+          cards: ['tuto', 'title', 'init', '6', '10', '12', '15', '18', '20', '24', '26', '30', '35', '38', '43', '46', '54', '59', '60', '75', '85', '95']
         }
       ]
     }
