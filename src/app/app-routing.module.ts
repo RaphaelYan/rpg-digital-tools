5920b6784d68ffcbc1e1e8109d5bc62cf200289d
Raphael Yan <raphael@donkeycode.com>
2020-04-22 16:58:41 +0200

    update to v9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,22 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: 'dd-builder',
-    loadChildren: './dd-builder/dd-builder.module#DdBuilderModule'
+    loadChildren: () => import('./dd-builder/dd-builder.module').then(m => m.DdBuilderModule)
   }, {
     path: 'dd-rules',
-    loadChildren: './dd-rules/dd-rules.module#DdRulesModule'
+    loadChildren: () => import('./dd-rules/dd-rules.module').then(m => m.DdRulesModule)
   }, {
     path: 'init',
-    loadChildren: './init/init.module#InitModule'
+    loadChildren: () => import('./init/init.module').then(m => m.InitModule)
   }, {
     path: 'resumes',
-    loadChildren: './resumes/resumes.module#ResumesModule'
+    loadChildren: () => import('./resumes/resumes.module').then(m => m.ResumesModule)
   }, {
     path: 'proba',
-    loadChildren: './proba/proba.module#ProbaModule'
+    loadChildren: () => import('./proba/proba.module').then(m => m.ProbaModule)
   }, {
     path: 'escape',
-    loadChildren: './escape/escape.module#EscapeModule'
+    loadChildren: () => import('./escape/escape.module').then(m => m.EscapeModule)
   }
 ];
 
