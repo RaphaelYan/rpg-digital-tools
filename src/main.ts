fc58f01747959cb6bcbe1604360839cd4952db7f
Raphael Yan <raphael@donkeycode.com>
2020-04-29 13:45:41 +0200

    back ga

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-
-  const html = `
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52202995-5"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-52202995-5');
-  </script>
-`;
-
-  document.write(html);
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
