e19ec8364fc023032b9385199ea81105cbf109e5
Raphael Yan <raphael@donkeycode.com>
2020-07-01 11:02:39 +0200

    logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 declare var $: any;
 
@@ -10,11 +13,48 @@ declare var $: any;
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(public angularFireAuth: AngularFireAuth) {
+  private logsCollection: AngularFirestoreCollection<any>;
+  public logs: Observable<any[]>;
+  private user: any;
+
+  constructor(
+    public angularFireAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+  ) {
   }
 
   public ngOnInit() {
     $('[data-toggle="tooltip"]').tooltip();
+
+    this.angularFireAuth.authState.subscribe((user) => {
+      if (!user) {
+        return;
+      }
+
+      this.user = user;
+
+      this.logsCollection = this.afs.collection<any>('logs', (ref) => {
+        return ref.orderBy('timestamp', 'desc');
+      });
+      this.logs = this.logsCollection.snapshotChanges().pipe(
+        map(actions => actions.map(a => {
+          const data = a.payload.doc.data() as any;
+          const id = a.payload.doc.id;
+          const date = new Date(data['timestamp']);
+
+          const timestampStr = date.getDate() + '/' + (1 + date.getMonth()) + ' ' + date.getHours() + ':' + date.getMinutes();
+          return { id, timestampStr, ...data };
+        }))
+      );
+
+      this.logsCollection.add({
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        timestamp: Date.now()
+      });
+    });
   }
 
   public login(): void {
@@ -23,5 +63,9 @@ export class AppComponent implements OnInit {
 
   public logout(): void {
     this.angularFireAuth.signOut();
+  }
+
+  public isAdmin(): boolean {
+    return this.user && this.user.email === 'maferyt@gmail.com';
   }
 }
